2db56fde1af3a199a620048d4c8335968709296e
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-06-09 19:55:35 +0200

    Added table syncdata_synchronize_tables

---
+++ vendor/phpManufaktur/SyncData/Data/SynchronizeTables.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * SyncData
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/SyncData
+ * @copyright 2013 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\SyncData\Data;
+
+use phpManufaktur\SyncData\Control\Application;
+
+class SynchronizeTables
+{
+    protected $app = null;
+    protected static $table_name = null;
+
+    /**
+     * Constructor
+     *
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+        self::$table_name = CMS_TABLE_PREFIX.'syncdata_synchronize_tables';
+    }
+
+    /**
+     * Create the table
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function createTable ()
+    {
+        $table = self::$table_name;
+        $SQL = <<<EOD
+    CREATE TABLE IF NOT EXISTS `$table` (
+      `id` INT(11) NOT NULL AUTO_INCREMENT,
+      `backup_id` VARCHAR(16) NOT NULL DEFAULT '',
+      `index_id` INT(11) NOT NULL DEFAULT '0',
+      `checksum` VARCHAR(32) NOT NULL DEFAULT '',
+      `table_name` VARCHAR(128) NOT NULL DEFAULT '',
+      `action` ENUM('UPDATE','INSERT','DELETE') NOT NULL DEFAULT 'UPDATE',
+      `content` MEDIUMTEXT,
+      `timestamp` TIMESTAMP,
+      PRIMARY KEY (`id`)
+    )
+    COMMENT='SyncData - table for tracking changed table records'
+    ENGINE=InnoDB
+    AUTO_INCREMENT=1
+    DEFAULT CHARSET=utf8
+    COLLATE='utf8_general_ci'
+EOD;
+        try {
+            $this->app['db']->query($SQL);
+            $this->app['monolog']->addInfo("Created table '".self::$table_name."' for the class BackupTables");
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw $e;
+        }
+    } // createTable()
+
+    /**
+     * Insert a new record into the table
+     *
+     * @param array $data
+     * @param string reference $id return the new ID
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function insert($data, &$id=null)
+    {
+        try {
+            $insert = array();
+            foreach ($data as $key => $value)
+                $insert[$this->app['db']->quoteIdentifier($key)] = is_string($value) ? $this->app['utils']->sanitizeText($value) : $value;
+            $this->app['db']->insert(self::$table_name, $insert);
+            $id = $this->app['db']->lastInsertId();
+        } catch (\Doctrine\DBAL\DBALException $e) {
+            throw $e;
+        }
+    }
+
+}
