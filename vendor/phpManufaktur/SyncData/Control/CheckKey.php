b51c522d30e8b0ba8daabc6c85f4824c8cacf012
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-07-30 05:28:31 +0200

    only small changes

--- vendor/phpManufaktur/SyncData/Control/CheckKey.php
+++ vendor/phpManufaktur/SyncData/Control/CheckKey.php
@@ -27,11 +27,21 @@ class CheckKey
         $this->app = $app;
     }
 
+    /**
+     * Get a hint to KEY usage
+     *
+     * @return string
+     */
     public function getKeyHint()
     {
         return "Action denied. Please authenticate with the key you have got.";
     }
 
+    /**
+     * Check if a KEY is needed and is valid
+     *
+     * @return boolean
+     */
     public function check()
     {
         if ($this->app['config']['security']['active'] === false) {
