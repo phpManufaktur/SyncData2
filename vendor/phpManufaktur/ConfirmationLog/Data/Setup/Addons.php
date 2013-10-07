5c3e1a8cadc08b252dfc2242c0c7ccb35fbd75c9
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-10-07 10:54:01 +0200

    corrected wrong @link in header information

--- vendor/phpManufaktur/ConfirmationLog/Data/Setup/Addons.php
+++ vendor/phpManufaktur/ConfirmationLog/Data/Setup/Addons.php
@@ -4,7 +4,7 @@
  * kitFramework::Basic
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://addons.phpmanufaktur.de/extendedWYSIWYG
+ * @link https://kit2.phpmanufaktur.de
  * @copyright 2013 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
--- vendor/phpManufaktur/SyncData/Control/Zip/unZipException.php
+++ vendor/phpManufaktur/SyncData/Control/Zip/unZipException.php
@@ -4,7 +4,7 @@
  * SyncData
  *
  * @author Team phpManufaktur <team@phpmanufaktur.de>
- * @link https://addons.phpmanufaktur.de/kitBase
+ * @link https://kit2.phpmanufaktur.de
  * @copyright 2013 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
